950d7afb8cab374ee9136ce91c4398fde4e3b97e
DIMA <Pecherskiy@vector-best.ru>
2024-06-06 09:00:01 +0700

    Копирование и сортировка массива.

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,20 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
 
+
+export function sortStrings(arr, param) {
+    let newArr = [...arr];
+    newArr.sort((a, b) => {
+        if (a.toLocaleLowerCase() === b.toLocaleLowerCase()) {
+            return a[0] !== a[0].toLocaleLowerCase() ? 1 : -1
+        } else {
+            return b.localeCompare(a)
+        }
+    }).reverse();
+    if (param == 'desc') {
+        newArr.reverse()
+    }
+
+    return [...newArr]
 }
